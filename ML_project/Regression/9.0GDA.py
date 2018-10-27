cf487a36b3dd73335fe9683502ec5fecdce6eec5
forever-xiaohao <1012958103@qq.com>
2018-10-27 21:50:13 +0800

    回归课程9.0

---
+++ ML_project/Regression/9.0GDA.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+@author: haoc
+@contact: 1012958103@qq.com
+@software: PyCharm
+@file: 9.0GDA.py
+@time: 2018/10/27 21:42
+"""
+
+
+def main():
+	pass
+
+
+if __name__ == "__main__":
+	main()
