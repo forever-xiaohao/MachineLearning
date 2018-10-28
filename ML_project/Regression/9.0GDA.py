10610e2eb6f4ad30334e4de6f36f5ffd6913e2c4
forever-xiaohao <1012958103@qq.com>
2018-10-28 16:21:26 +0800

    平方根计算

--- ML_project/Regression/9.0GDA.py
+++ ML_project/Regression/9.0GDA.py
@@ -8,11 +8,14 @@
 @file: 9.0GDA.py
 @time: 2018/10/27 21:42
 """
+import math
 
-
-def main():
-	pass
 
 
 if __name__ == "__main__":
-	main()
+	learning_rate = 0.01
+	for a in range(1, 100):
+		cur  = 0
+		for i in range(1000):
+			cur -= learning_rate*(cur**2 - a)
+		print ' %d的平方根（近似）为：%.8f,真实值是：%.8f' %(a, cur, math.sqrt(a))
