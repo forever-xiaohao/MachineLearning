1c195ccf71ceff20a7eb74625d6b41ab97c977cb
forever-xiaohao <1012958103@qq.com>
2018-11-14 11:24:32 +0800

    决策树学习（一）

---
+++ ML_project/RandomForest/11.2.Iris_DecisionTree_Enum.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+"""
+@author: ThinkPad
+@contact: 1012958103@qq.com
+@software: PyCharm
+@file: 11.2.Iris_DecisionTree_Enum.py
+@time: 2018/11/14 11:23
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
